a0b61f0404f18f02a97e0b19cdbb42f00b5e84f0
omihalcea <omihalcea@iesebre.com>
2024-12-22 23:42:36 +0100

    correcio

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,7 +3,7 @@
         <div class="flex lg:flex-1">
             <a href="/" class="-m-1.5 p-1.5">
                 <span class="sr-only">Your Company</span>
-                <img src="geto.jpg" alt="JetBrains logo." class="logo w-32 h-32 rounded-full">
+                <img src="/geto.jpg" alt="JetBrains logo." class="logo w-32 h-32 rounded-full">
             </a>
         </div>
         <div id="menu-button" class="sm:hidden">
