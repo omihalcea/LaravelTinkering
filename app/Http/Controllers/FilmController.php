1330cd59761e44ba73d77a953a443c0bdbfed84e
omihalcea <omihalcea@iesebre.com>
2024-12-22 23:31:25 +0100

    Acabat correcio

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -15,18 +15,12 @@ class FilmController extends Controller
         $films = Film::all();
         return view('films.index', compact('films'));
     }
-    /**
-     * Show the form for creating a new resource.
-     */
 
     public function create()
     {
         return view('films.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request, Film $film)
     {
         $validated = $request->validate([
@@ -41,28 +35,18 @@ class FilmController extends Controller
         return redirect()->route('films.index')->with('success', 'Film created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show($id)
     {
         $film = Film::findOrFail($id);
         return view('films.show', compact('film'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit($id)
     {
         $film = Film::findOrFail($id);
         return view('films.edit', compact('film'));
     }
 
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Film $film)
     {
         $validated = $request->validate([
